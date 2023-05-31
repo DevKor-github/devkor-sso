76d89a662459faae0c979e82c78614bc7b8ec3bd
overthestream <njhnjh02@naver.com>
2023-05-31 15:54:45 +0900

    infra: create shell scripts

---
+++ src/main/java/devkor/sso/profile/ProfileController.java
@@ -0,0 +1,28 @@
+package devkor.sso.profile;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class ProfileController {
+    private final Environment env;
+
+    @GetMapping("/profile")
+    public String getProfile() {
+        List<String> profiles = Arrays.asList(env.getActiveProfiles());
+
+        List<String> deployProfiles = Arrays.asList("blue", "green");
+        String defaultProfile = profiles.isEmpty() ? "default" : profiles.get(0);
+
+        return profiles.stream()
+                .filter(deployProfiles::contains)
+                .findAny()
+                .orElse(defaultProfile);
+    }
+}
